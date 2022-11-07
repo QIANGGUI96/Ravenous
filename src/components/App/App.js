ce3e46c767b7115a550cff8ef826cfb1e2edf967
Qiang <qianggui96@icloud.com>
2022-11-07 22:06:55 +0900

    changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,33 @@
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
-    <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar />
-    <BusinessList />
-  </div>
-  );
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
 }
 
+const businesses=[business,business,business,business,business,business]
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList businesses={businesses}/>
+    </div>
+
+    )
+  }
+};
 
 export default App;
